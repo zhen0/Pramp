c378475bade7a7327af8842d856d9ce6297cf54b
Jenny <jenniferhelengrange@gmail.com>
2019-06-13 16:02:49 -0400

    solves islandCount with jest tests

--- islandCount.js
+++ islandCount.js
@@ -1,5 +1,4 @@
 function islandCount(binaryMatrix) {
-  // your code goes here
   let islands = 0;
   let rows = binaryMatrix.length;
   let cols = binaryMatrix[0].length;
@@ -7,6 +6,7 @@ function islandCount(binaryMatrix) {
   for (let i = 0; i < rows; i++) {
     for (let j = 0; j < cols; j++) {
       let val = binaryMatrix[i][j];
+
       if (val === 1) {
         islands++;
         switchTo2(binaryMatrix, i, j, rows, cols);
@@ -19,18 +19,36 @@ function islandCount(binaryMatrix) {
 function switchTo2(binaryMatrix, row, col, rows, cols) {
   binaryMatrix[row][col] = 2;
   if (row > 0) {
-    binaryMatrix[row - 1][col] = 2;
+    if (binaryMatrix[row - 1][col] === 1) {
+      switchTo2(binaryMatrix, row - 1, col, rows, cols);
+    } else {
+      binaryMatrix[row - 1][col] = 2;
+    }
   }
   if (row < rows - 1) {
-    binaryMatrix[row + 1][col] = 2;
+    if (binaryMatrix[row + 1][col] === 1) {
+      switchTo2(binaryMatrix, row + 1, col, rows, cols);
+    } else {
+      binaryMatrix[row + 1][col] = 2;
+    }
   }
   if (col < cols - 1) {
-    binaryMatrix[row][col + 1] = 2;
+    if (binaryMatrix[row][col + 1] === 1) {
+      switchTo2(binaryMatrix, row, col + 1, rows, cols);
+    } else {
+      binaryMatrix[row][col + 1] = 2;
+    }
   }
   if (col > 0) {
-    binaryMatrix[row][col - 1] = 2;
+    if (binaryMatrix[row][col - 1] === 1) {
+      switchTo2(binaryMatrix, row, col - 1, rows, cols);
+    } else {
+      binaryMatrix[row][col - 1] = 2;
+    }
   }
   console.log(binaryMatrix);
 }
+
+console.log(islandCount([[1, 0, 1, 0]]));
 
 module.exports = islandCount;
--- islandCount.test.js
+++ islandCount.test.js
@@ -1,5 +1,13 @@
 const islandCount = require("./islandCount");
 
-test("[0,1,0] to return 1", () => {
-  expect(islandCount([0, 1, 0])).toBe(1);
+test("[[1, 0,1,0]] to return 2", () => {
+  expect(islandCount([[1, 0, 1, 0]])).toBe(2);
+});
+
+test("[[1,0,1,0],[0,1,1,1],[0,0,1,0]] to return 2", () => {
+  expect(islandCount([[1, 0, 1, 0], [0, 1, 1, 1], [0, 0, 1, 0]])).toBe(2);
+});
+
+test("[[1, 0,1,0]] to return 2", () => {
+  expect(islandCount([[1, 0, 1, 0]])).toBe(2);
 });
